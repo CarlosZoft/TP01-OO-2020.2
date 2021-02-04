b0bc92bdae16cfd71eb86b0eced0caa4a989e52f
Carlos <carlosoftwar@gmail.com>
2021-02-04 17:20:34 -0300

    Update data methods and atributes

--- src/application/Data.java
+++ src/application/Data.java
@@ -1,19 +1,26 @@
 package application;
 import java.util.ArrayList;
 
+/**
+ * Class de dados
+ */
 public class Data {
     private ArrayList <Double> dados = new ArrayList<Double>();
 
+
+    // Setar dados do ArrayList
     public void setDados(double dados) {
         this.dados.add(dados);
     }
-
+    // Pegar dados do ArrayList
     public ArrayList<Double> getDados() {
         return this.dados;
     }
+    // Listar dados no console
     public void listaDados() {
-        for(int i=0;i < dados.size(); i++){
-            System.out.println(dados.get(i));
+        for(int i=0 ;i < dados.size(); i++){
+            if(i != 0)System.out.print(","+ dados.get(i));
+            else System.out.print(dados.get(i));
         }
     }
 }
