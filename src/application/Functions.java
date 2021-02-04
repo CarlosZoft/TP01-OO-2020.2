7cd906314968bbebfe1a2642be4880cba0186252
Carlos <carlosoftwar@gmail.com>
2021-02-04 20:29:52 -0300

    Update: taking punctuation to avoid mistakes

--- src/application/Functions.java
+++ src/application/Functions.java
@@ -36,18 +36,19 @@ public class Functions {
      * print mensagens para o úsuario;
      */
     public static void menu(){
-        System.out.println("O QUE DESEJA CONSULTAR ?\n");
-        System.out.println("1 - Cálculo da temperatura média");
-        System.out.println("2 - Cálculo da temperatura mínima");
-        System.out.println("3 - Cálculo da temperatura máxima");
-        System.out.println("4 - Relatório meteorológico\n");
-        System.out.println("DIGITE QUALQUER NUMERO DIFERENTE PARA CANCELAR");
+        System.out.println("\nO QUE DESEJA CONSULTAR ?\n");
+        System.out.println("1 - Calculo da temperatura media");
+        System.out.println("2 - Calculo da temperatura minima");
+        System.out.println("3 - Calculo da temperatura maxima");
+        System.out.println("4 - Relatorio meteorologico");
+        System.out.println("0 - Para consultar outra data/Cancelar");
+        System.out.println("____________________________________________________________");
     }
     public static void msg (int a, int decisao){
         if(decisao<=0 || decisao > 4)System.exit(0);
         switch (a){
             case 1:
-                System.out.print("Digite o Mês(1-12):");
+                System.out.print("Digite o Mes(1-12):");
                 break;
             case 2:
                 System.out.print("Digite o Ano (2077):");
