ec139f078016c93d9950fe5bac8ef9a467b80cc6
Carlos <carlosoftwar@gmail.com>
2021-02-04 17:21:46 -0300

    update calc: new methods

--- src/application/Calc.java
+++ src/application/Calc.java
@@ -1,6 +1,19 @@
 package application;
+import java.util.ArrayList;
 
+/**
+ * Class Calc dias;
+ */
 public class Calc {
 
+    private ArrayList <Integer> day = new ArrayList<Integer>();
 
+    //Setar dias de Menor ou Maior média
+    public void setDays(int day) {
+        this.day.add(day);
+    }
+    //Pegar dias setados
+    public ArrayList<Integer> getDays(){
+        return this.day;
+    }
 }
