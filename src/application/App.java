05495fc5d1ef7bf2645412e7e752bcf398787b06
Carlos <carlosoftwar@gmail.com>
2021-02-04 20:28:42 -0300

    Update App: taking punctuation to avoid mistakes

--- src/application/App.java
+++ src/application/App.java
@@ -26,7 +26,7 @@ public class App {
             ano = scan.nextInt();
         //Quantidade de dias(Mês);
         days = aux.days(ano,mes);
-        System.out.printf("Agora digite as média(s) de temperatura(s) dos %d dia(s)! Ex: 29,7\n", days);
+        System.out.printf("Agora digite as medias de temperatura dos %d dias. Ex:29,7\n", days);
         Data dados = new Data();
         //leitura dias
         while(cont <= days){
@@ -40,30 +40,34 @@ public class App {
         /**
          * Saida de dados relativo ao tipo de pesquisa
          */
-        switch (decisao){
-            case 1:
-                System.out.printf("A temperatura média do mês informado é: %.2f °C", aux.media(dados.getDados()));
-                break;
-            case 2:
-                System.out.printf("A temperatura mínima do mês informado é :%.2f °C no(s) dia(s):\n", menor);
-                Calc listDays = aux.minMax(dados.getDados(),menor);
-                aux.printDays(listDays.getDays(),mes, ano);
-                break;
-            case 3:
-                System.out.printf("A temperatura mínima do mês informado é :%.2f °C no(s) dia(s):\n", maior);
-                listDays = aux.minMax(dados.getDados(),maior);
-                aux.printDays(listDays.getDays(),mes, ano);
-                break;
-            case 4:
-                System.out.printf("Média de temperadaturas do dia 1 ao %d ,respectivamente:\n", days);
-                dados.listaDados();
-                System.out.printf("\nMédia de temperadatura do período %d/%d : %.2f °C\n", mes,ano,aux.media(dados.getDados()));
-                System.out.printf("Mínima de temperadatura do período %d/%d : %.1f °C\n", mes,ano,menor);
-                System.out.printf("Máxima de temperadatura do período %d/%d : %.1f °C\n", mes,ano,maior);
-                break;
-            default:
-                System.out.println("Volte Sempre!");
-                break;
+        while(decisao!=0){
+            switch (decisao) {
+                case 1:
+                    System.out.printf("Temperatura media do mes informado : %.2f °C\n", aux.media(dados.getDados()));
+                    break;
+                case 2:
+                    System.out.printf("Temperatura minima do mes informado : %.2f °C no(s) dia(s):\n", menor);
+                    Calc listDays = aux.minMax(dados.getDados(), menor);
+                    aux.printDays(listDays.getDays(), mes, ano);
+                    break;
+                case 3:
+                    System.out.printf("Temperatura maxima do mes informado : %.2f °C no(s) dia(s):\n", maior);
+                    listDays = aux.minMax(dados.getDados(), maior);
+                    aux.printDays(listDays.getDays(), mes, ano);
+                    break;
+                case 4:
+                    System.out.printf("Medias de temperadaturas do dia 1 ao %d ,respectivamente:\n", days);
+                    dados.listaDados();
+                    System.out.printf("\nMedia de temperadatura do periodo %d/%d : %.2f °C\n", mes, ano, aux.media(dados.getDados()));
+                    System.out.printf("Minima de temperadatura do periodo %d/%d : %.1f °C\n", mes, ano, menor);
+                    System.out.printf("Maxima de temperadatura do periodo %d/%d : %.1f °C\n", mes, ano, maior);
+                    break;
+                default:
+                    System.out.println("Opcao invalida");
+                    break;
+            }
+                aux.menu();
+                decisao = scan.nextInt();
         }
     }
 }
