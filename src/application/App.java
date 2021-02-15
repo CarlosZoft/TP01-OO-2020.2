0b3b2cbdc32d4d9cdd8c97dba7f9e5411af85147
Carlos <carlosoftwar@gmail.com>
2021-02-14 23:38:05 -0300

    Update : Validando Mes,Ano e Opcao menu;

--- src/application/App.java
+++ src/application/App.java
@@ -1,62 +1,74 @@
 package application;
-import java.util.ArrayList;
+
 import java.util.Scanner;
-import java.time.YearMonth;
-import java.util.Collections;
-import java.io.IOException;
+
 
 
 public class App {
     /**
      *  Função Main;
-     * @param args
+     * @param args;
      */
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Functions aux = new Functions();
 
-        int mes,ano,decisao,days,cont=1;
+        int mes, ano, decisao,days;
+
         double a,menor=1000, maior=-1000;
-        aux.menu();
-        decisao = scan.nextInt();
+        aux.menu(0,0);
+            decisao = scan.nextInt();
+        while(decisao < 0 || decisao > 4){
+            System.out.println("Opcao invalida! 1-4 ou 0");
+            aux.menu(0,0);
+            decisao = scan.nextInt();
+        }
         aux.msg(1,decisao);
+        mes = scan.nextInt();
+        while(mes < 1 || mes > 12){
+            System.out.println("Mes invalido! 1-12");
+            aux.msg(1,decisao);
             mes = scan.nextInt();
+        }
+
         aux.msg(2,decisao);
+        ano = scan.nextInt();
+        while(ano < 2011 || ano > 2020){
+            System.out.println("Ano invalido! 2011-2020");
+            aux.msg(2,decisao);
             ano = scan.nextInt();
+        }
         //Quantidade de dias(Mês);
         days = aux.days(ano,mes);
         System.out.printf("Agora digite as medias de temperatura dos %d dias. Ex:29,7\n", days);
         Data dados = new Data();
         //leitura dias
-        while(cont <= days){
-            System.out.printf("dia %d : ", cont);
+        for(int i=1 ; i<= days ; i++){
+            System.out.printf("dia %d : ", i);
             a = scan.nextDouble();
             dados.setDados(a);
             if(a<menor)menor=a;
             else if(a>maior)maior=a;
-            cont++;
         }
-        /**
-         * Saida de dados relativo ao tipo de pesquisa
-         */
+        //Saida de dados relativo ao tipo de pesquisa
         while(decisao!=0){
             switch (decisao) {
                 case 1:
-                    System.out.printf("Temperatura media do mes informado : %.2f °C\n", aux.media(dados.getDados()));
+                    System.out.printf("\nTemperatura media do mes informado : %.2f °C\n", aux.media(dados.getDados()));
                     break;
                 case 2:
-                    System.out.printf("Temperatura minima do mes informado : %.2f °C no(s) dia(s):\n", menor);
+                    System.out.printf("\nTemperatura minima do mes informado : %.2f °C no(s) dia(s):\n", menor);
                     Calc listDays = aux.minMax(dados.getDados(), menor);
                     aux.printDays(listDays.getDays(), mes, ano);
                     break;
                 case 3:
-                    System.out.printf("Temperatura maxima do mes informado : %.2f °C no(s) dia(s):\n", maior);
+                    System.out.printf("\nTemperatura maxima do mes informado : %.2f °C no(s) dia(s):\n", maior);
                     listDays = aux.minMax(dados.getDados(), maior);
                     aux.printDays(listDays.getDays(), mes, ano);
                     break;
                 case 4:
-                    System.out.printf("Medias de temperaturas do dia 1 ao %d ,respectivamente:\n", days);
+                    System.out.printf("\nMedias de temperaturas do dia 1 ao %d ,respectivamente:\n", days);
                     dados.listaDados();
                     System.out.printf("\nDados obtidos do periodo %d/%d\n", mes, ano);
                     System.out.printf("Media : %.2f °C\n", aux.media(dados.getDados()));
@@ -67,8 +79,14 @@ public class App {
                     System.out.println("Opcao invalida");
                     break;
             }
-                aux.menu();
+            aux.menu(ano,mes);
+            decisao = scan.nextInt();
+            while(decisao < 0 || decisao > 4){
+                System.out.println("Opcao invalida! Digite novamente : ");
+                aux.menu(ano,mes);
                 decisao = scan.nextInt();
+            }
+            if(decisao == 0)System.exit(0);
         }
     }
 }
